29340f27f5e390c282cf1fd0efc7356b2c86385e
tgomilar <tanja.gomilar@gmail.com>
2023-02-09 13:28:32 +0100

    first commit

---
+++ _app/immutable/chunks/1-f9741638.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-02d0268a.js";export{t as component};
---
+++ _app/immutable/chunks/2-9ee25bc3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-d1ed6c9f.js";export{t as component};
